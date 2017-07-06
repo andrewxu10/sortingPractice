28c42a644f657c1a1622947d9aca40b29eef18a1
Andrew Xu <andrewxu_2016@depauw.edu>
2017-07-06 12:50:10 -0700

    july 6 start

--- src/sorting_Homework/mergeSort.java
+++ src/sorting_Homework/mergeSort.java
@@ -5,9 +5,62 @@ public class mergeSort {
 	
 	public static void main(String[] args) {
 		int [] x= {8,2,53,2,8,6,34,76};
-		int [] y= mSort(x);
+		int [] y= mergeySort(x);
 		System.out.println(Arrays.toString(y));
 	}
+	public static int[] mergeySort(int[] x) {
+		if(x.length == 1) {
+			return x;
+		}
+		return mergeymerge(mergeySort(Arrays.copyOfRange(x, 0, x.length/2)), mergeySort(Arrays.copyOfRange(x, x.length/2, x.length)));
+	}
+	public static int[] mergeymerge(int[] a, int b[]) {
+		int i = 0; int j = 0; int z = 0; int[] ret = new int[a.length + b.length];
+		while(i < a.length && j < b.length) {
+			if(a[i] < b[j]) {
+				ret[z] = a[i];
+				z++; i++;
+			} else { 
+				ret[z] = b[j];
+				z++; j++;
+			}
+		}
+		if(i == a.length) {
+			while(j<b.length) {
+				ret[z] = b[j];
+				z++; j++;
+			}
+		}
+		if(j == b.length) {
+			while(i<a.length) {
+				ret[z] = a[i];
+				z++; i++;
+			}
+		}
+		return ret;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	public static int[] mS(int[] x){ //7.3.17 practice
 		if(x.length == 1) {
